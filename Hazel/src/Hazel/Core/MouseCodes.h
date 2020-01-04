b40ad7405615f1951ad0137b0baa240cd6b0b659
Luis Hernandez <luishdz_21@hotmail.com>
2020-01-04 00:57:19 -0600

    Implement KeyCodes and MouseCodes enums. -Macro defines now use the enums as well.

---
+++ Hazel/src/Hazel/Core/MouseCodes.h
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace Hazel
+{
+	typedef enum class MouseCode : uint16_t
+	{
+		// From glfw3.h
+		Button0				= 0,
+		Button1				= 1,
+		Button2				= 2,
+		Button3				= 3,
+		Button4				= 4,
+		Button5				= 5,
+		Button6				= 6,
+		Button7				= 7,
+		ButtonLast			= Button7,
+		ButtonLeft			= Button0,
+		ButtonRight			= Button1,
+		ButtonMiddle		= Button2
+	} Mouse;
+
+	inline std::ostream& operator<<(std::ostream& os, MouseCode mouseCode)
+	{
+		os << static_cast<int32_t>(mouseCode);
+		return os;
+	}
+}
+
+// From glfw3.h
+#define HZ_MOUSE_BUTTON_1			::Hazel::MouseCode::Button0
+#define HZ_MOUSE_BUTTON_2			::Hazel::MouseCode::Button1
+#define HZ_MOUSE_BUTTON_3			::Hazel::MouseCode::Button2
+#define HZ_MOUSE_BUTTON_4			::Hazel::MouseCode::Button3
+#define HZ_MOUSE_BUTTON_5			::Hazel::MouseCode::Button4
+#define HZ_MOUSE_BUTTON_6			::Hazel::MouseCode::Button5
+#define HZ_MOUSE_BUTTON_7			::Hazel::MouseCode::Button6
+#define HZ_MOUSE_BUTTON_8			::Hazel::MouseCode::Button7
+#define HZ_MOUSE_BUTTON_LAST		::Hazel::MouseCode::ButtonLast
+#define HZ_MOUSE_BUTTON_LEFT		::Hazel::MouseCode::ButtonLeft
+#define HZ_MOUSE_BUTTON_RIGHT		::Hazel::MouseCode::ButtonRight
+#define HZ_MOUSE_BUTTON_MIDDLE		::Hazel::MouseCode::ButtonMiddle
